51a364df2c036ed33d31e31b85120a98bf1f45a2
William D'Onofrio <williamdonofrio+git@gmail.com>
2023-05-03 19:50:40 -0600

    Add simple menu to app

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.properties import (
     NumericProperty, ReferenceListProperty, ObjectProperty
 )
 from kivy.vector import Vector
 from kivy.clock import Clock
 
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 
 class PongPaddle(Widget):
     score = NumericProperty(0)
@@ -62,14 +65,41 @@ class PongGame(Widget):
         if touch.x > self.width - self.width / 3:
             self.player2.center_y = touch.y
 
+    def start_game(self, *args):
+        self.serve_ball()
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+    def quit_game(self, *args):
+        App.get_running_app().stop()
+
+class MainMenu(FloatLayout):
+    def __init__(self, **kwargs):
+        super(MainMenu, self).__init__(**kwargs)
+
+        self.label = Label(text='PONG GAME', font_size=50, size_hint=(1, 0.2), pos_hint={'top': 1})
+        self.add_widget(self.label)
+
+        self.play_button = Button(text='PLAY', font_size=30, size_hint=(0.5, 0.2), pos_hint={'x': 0.25, 'y': 0.4})
+        self.play_button.bind(on_release=self.start_pong)
+        self.add_widget(self.play_button)
+
+        self.quit_button = Button(text='QUIT', font_size=30, size_hint=(0.5, 0.2), pos_hint={'x': 0.25, 'y': 0.2})
+        self.quit_button.bind(on_release=self.quit_game)
+        self.add_widget(self.quit_button)
+
+    def start_pong(self, *args):
+        game = PongGame()
+        App.get_running_app().root.clear_widgets()
+        App.get_running_app().root.add_widget(game)
+        game.start_game()
+
+    def quit_game(self, *args):
+        App.get_running_app().stop()
 
 class PongApp(App):
     def build(self):
-        game = PongGame()
-        game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
-
+        menu = MainMenu()
+        return menu
 
 if __name__ == '__main__':
     PongApp().run()
