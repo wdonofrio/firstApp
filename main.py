01e8663fdd225b8496e9e92a2876da0eeabd30f7
William D'Onofrio <williamdonofrio+git@gmail.com>
2023-05-10 20:50:12 -0600

    Modify pong for single player Bricks

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
+from kivy.graphics.texture import Texture
+
+class Brick(Widget):
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
+
 class PongPaddle(Widget):
     score = NumericProperty(0)
 
@@ -29,15 +40,21 @@ class PongBall(Widget):
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+        self.pos = Vector(*self.velocity)*2 + self.pos
 
 
 class PongGame(Widget):
     ball = ObjectProperty(None)
-    player1 = ObjectProperty(None)
-    player2 = ObjectProperty(None)
+    player = ObjectProperty(None)
+    brick = ObjectProperty(None)
 
-    def serve_ball(self, vel=(4, 0)):
+    # def set_bricks(self):
+    #     for i in range(10):
+    #         brick = Brick()
+    #         brick.pos = (i * 50, 250)
+    #         self.add_widget(brick)
+
+    def serve_ball(self, vel=(0, 4)):
         self.ball.center = self.center
         self.ball.velocity = vel
 
@@ -45,28 +62,27 @@ class PongGame(Widget):
         self.ball.move()
 
         # bounce of paddles
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
+        self.player.bounce_ball(self.ball)
+        # self.brick.bounce_ball(self.ball)
 
         # bounce ball off bottom or top
-        if (self.ball.y < self.y) or (self.ball.top > self.top):
+        if self.ball.top > self.top:
             self.ball.velocity_y *= -1
+        elif self.ball.x < self.x:  # ball hit the left wall
+            self.ball.velocity_x *= -1
+        elif self.ball.right > self.right:  # ball hit the right wall
+            self.ball.velocity_x *= -1
 
-        # went of to a side to score point?
-        if self.ball.x < self.x:
-            self.player2.score += 1
-            self.serve_ball(vel=(4, 0))
-        if self.ball.right > self.width:
-            self.player1.score += 1
-            self.serve_ball(vel=(-4, 0))
+        # ball falls to through the bottom
+        if self.ball.y < self.y-50:
+            exit(0)
 
     def on_touch_move(self, touch):
-        if touch.x < self.width / 3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width / 3:
-            self.player2.center_y = touch.y
+        if touch.y < self.width / 3:
+            self.player.center_x = touch.x
 
     def start_game(self, *args):
+        # self.set_bricks()
         self.serve_ball()
         Clock.schedule_interval(self.update, 1.0 / 60.0)
 
